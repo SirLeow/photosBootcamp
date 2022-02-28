21c5e91369cc4b965f59b875e09bb85a7bdcade0
Thiago Barros <t.b.lima18@gmail.com>
2022-02-28 16:40:42 -0300

    Development of the pickImageFromGalery function and final changes

--- app/src/main/java/com/example/photosbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/photosbootcamp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.photosbootcamp
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -26,7 +28,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pickImageFromGalery() {
-        TODO("Not yet implemented")
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            image_view.setImageURI(data?.data)
+        }
     }
 
     override fun onRequestPermissionsResult(
