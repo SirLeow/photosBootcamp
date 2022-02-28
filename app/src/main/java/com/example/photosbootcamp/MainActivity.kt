bd4ba2c07408c2c5b68b5742fd81fb8b997f3372
Thiago Barros <t.b.lima18@gmail.com>
2022-02-28 16:53:09 -0300

    Added preparation for the use of the camera(Permissions and a Button)

--- app/src/main/java/com/example/photosbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/photosbootcamp/MainActivity.kt
@@ -11,6 +11,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSION_CODE_IMAGE_PICK = 1000
+        private const val IMAGE_PICK_CODE = 1001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                   requestPermissions(permission, PERMISSION_CODE)
+                   requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
                 } else pickImageFromGalery()
             }
             else pickImageFromGalery()
@@ -47,7 +53,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when(requestCode ) {
-            PERMISSION_CODE -> {
+            PERMISSION_CODE_IMAGE_PICK -> {
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     pickImageFromGalery()
                 } else {
@@ -55,9 +61,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-    companion object {
-        private val PERMISSION_CODE = 1000
-        private val IMAGE_PICK_CODE = 1001
     }
 }
